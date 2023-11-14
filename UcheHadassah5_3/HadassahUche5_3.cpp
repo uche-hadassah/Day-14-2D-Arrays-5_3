5da4167b4774c6a129c6a1c74de5c2645259cd66
Hadassah Uche <uhadassah1@gmail.com>
2023-11-14 19:45:32 +0200

    Cleaned up the code and added comment

--- UcheHadassah5_3/HadassahUche5_3.cpp
+++ UcheHadassah5_3/HadassahUche5_3.cpp
@@ -1,25 +1,26 @@
-/*Modify the Example program 2 of slide 45 (Topic 2 Arrays) so that it also gives the 
-sum of each column (as well as the sum of each row). */
+/*Name:Uche Hadassah
+This program receives  a 3x3 array from the user, and outputs two tables;One with the totals of each row 
+and another with the totals of each column*/
 #include <iostream>
 using namespace std;
 int main()
 {
-	const int rows = 3;
-	const int columns = 3;
-	int A[rows][columns];
+	const int ROW = 3;
+	const int COLUMN = 3;
+	int A[ROW][COLUMN];
 	int i, j, totalr = 0,totalc = 0;
-	cout << "Enter " << rows * columns << " integers, " << columns << " per row: " <<endl;
-		for (i = 0; i < rows; i++)
+	cout << "Enter " << ROW * COLUMN << " integers, " << COLUMN << " per row: " <<endl;
+		for (i = 0; i < ROW; i++)//Receives the array from the user by row 
 		{
 			cout << "Row " << i << " : " << endl;
-			for (j = 0; j < columns; j++)
+			for (j = 0; j < COLUMN; j++)
 				cin >> A[i][j];
 		}
 	cout << "Table with the totals of each row is: " << endl;
-	for (i = 0; i < rows; i++)
+	for (i = 0; i < ROW; i++)
 	{
 		totalr = 0; //reset total at the beginning of each row
-		for (j = 0; j < columns; j++)
+		for (j = 0; j < COLUMN; j++)
 		{
 			cout << A[i][j] << " ";
 			totalr = totalr + A[i][j]; //add to total
@@ -27,10 +28,10 @@ int main()
 		cout << " = " << totalr << endl; //print total
 	}
 	cout << "Table with the totals of each column is: " << endl;
-	for (i = 0; i < rows; i++)
+	for (i = 0; i < ROW; i++)
 	{
-		totalc = 0; //reset total at the beginning of each row
-		for (j = 0; j < columns; j++)
+		totalc = 0; //reset total at the beginning of each column
+		for (j = 0; j < COLUMN; j++)
 		{
 			cout << A[j][i] << " ";
 			totalc = totalc + A[j][i]; //add to total
